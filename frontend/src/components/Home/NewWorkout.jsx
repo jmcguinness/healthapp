ea6ef7711559c87fda8924af49186d402352fd3a
jmcguinness <27508060+jmcguinness@users.noreply.github.com>
2024-07-03 09:01:52 -0400

    Strava API build and backend model updates

--- frontend/src/components/Home/NewWorkout.jsx
+++ frontend/src/components/Home/NewWorkout.jsx
@@ -1,12 +1,49 @@
 import { Container, Card, CardHeader, Button, Text, Input } from '@chakra-ui/react'
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function NewWorkout () {
 
+    {/* Button Variable */}
     const [newWorkout, setNewWorkout] = useState(false)
     const [typeSelected, setTypeSelected] = useState(false)
     const [runSelected, setRunSelected] = useState(false)
     const [liftSelected, setLiftSelected] = useState(false)
+    
+    {/* Form Variables */}
+    const [workoutSelected, setWorkoutSelected] = useState('')
+    const [runDetails, setRunDetails] = useState('')
+    const [lift1, setLift1] = useState('')
+    const [lift1Details, setLift1Details] = useState('')
+    const [lift2, setLift2] = useState('')
+    const [lift2Details, setLift2Details] = useState('')
+    const [lift3, setLift3] = useState('')
+    const [lift3Details, setLift3Details] = useState('')
+    const [lift4, setLift4] = useState('')
+    const [lift4Details, setLift4Details] = useState('')
+    const [lift5, setLift5] = useState('')
+    const [lift5Details, setLift5Details] = useState('')
+    const [lift6, setLift6] = useState('')
+    const [lift6Details, setLift6Details] = useState('')
+
+    {/* Render Variables */}
+    const [renderWorkoutSelected, setRenderWorkoutSelected] = useState('')
+    const [renderRunDetails, setRenderRunDetails] = useState('')
+    const [renderLift1, setRenderLift1] = useState('')
+    const [renderLift1Details, setRenderLift1Details] = useState('')
+    const [renderLift2, setRenderLift2] = useState('')
+    const [renderLift2Details, setRenderLift2Details] = useState('')
+    const [renderLift3, setRenderLift3] = useState('')
+    const [renderLift3Details, setRenderLift3Details] = useState('')
+    const [renderLift4, setRenderLift4] = useState('')
+    const [renderLift4Details, setRenderLift4Details] = useState('')
+    const [renderLift5, setRenderLift5] = useState('')
+    const [renderLift5Details, setRenderLift5Details] = useState('')
+    const [renderLift6, setRenderLift6] = useState('')
+    const [renderLift6Details, setRenderLift6Details] = useState('')
+    
+
+    {/* Handling buttons within new workout panel */}
 
     const handleNewWorkout = () => {
         setNewWorkout(true)
@@ -15,11 +52,13 @@ function NewWorkout () {
     const handleRun = () => {
         setTypeSelected(true)
         setRunSelected(true)
+        setWorkoutSelected('Run')
     }
 
     const handleLift = () => {
         setTypeSelected(true)
         setLiftSelected(true)
+        setWorkoutSelected('Lift')
     }
 
     const handleSubmit = () => {
@@ -27,6 +66,7 @@ function NewWorkout () {
         setTypeSelected(false)
         setRunSelected(false)
         setLiftSelected(false)
+        postNewWorkout()
     }
 
     const handleBackNewWorkout = () => {
@@ -39,19 +79,98 @@ function NewWorkout () {
         setRunSelected(false)
     }
 
-    if (newWorkout == false) {
+    const getMostRecentWorkout = async () => {
+
+        await axios({
+            method: 'get',
+            url: 'http://127.0.0.1:8000/api/newWorkout'
+        }).then((response) => {
+            console.log(response.data)
+            setRenderWorkoutSelected(response.data[response.data.length-1].workoutSelected)
+            setRenderRunDetails(response.data[response.data.length-1].runDetails)
+            setRenderLift1(response.data[response.data.length-1].lift1)
+            setRenderLift1Details(response.data[response.data.length-1].lift1Details)
+            setRenderLift2(response.data[response.data.length-1].lift2)
+            setRenderLift2Details(response.data[response.data.length-1].lift2Details)
+            setRenderLift3(response.data[response.data.length-1].lift3)
+            setRenderLift3Details(response.data[response.data.length-1].lift3Details)
+            setRenderLift4(response.data[response.data.length-1].lift4)
+            setRenderLift4Details(response.data[response.data.length-1].lift4Details)
+            setRenderLift5(response.data[response.data.length-1].lift5)
+            setRenderLift5Details(response.data[response.data.length-1].lift5Details)
+            setRenderLift6(response.data[response.data.length-1].lift6)
+            setRenderLift6Details(response.data[response.data.length-1].lift6Details)
+
+        })
+    }
+
+    const postNewWorkout = async () => {
+
+        let workoutData = new FormData()
+
+        workoutData.append('workoutSelected', workoutSelected)
+        workoutData.append('runDetails', runDetails)
+        workoutData.append('lift1', lift1)
+        workoutData.append('lift1Details', lift1Details)
+        workoutData.append('lift2', lift2)
+        workoutData.append('lift2Details', lift2Details)
+        workoutData.append('lift3', lift3)
+        workoutData.append('lift3Details', lift3Details)
+        workoutData.append('lift4', lift4)
+        workoutData.append('lift4Details', lift4Details)
+        workoutData.append('lift5', lift5)
+        workoutData.append('lift5Details', lift5Details)
+        workoutData.append('lift6', lift6)
+        workoutData.append('lift6Details', lift6Details)
+
+        await axios({
+            method: 'post',
+            url: 'http://127.0.0.1:8000/api/newWorkout',
+            data: workoutData
+        }).then((response) => {
+            console.log(response)
+        })
+    }
+
+    const pageLoad = () => {
+        getMostRecentWorkout()
+    }
+
+    useEffect(() => {
+        pageLoad();
+    }, [])
+
+    if (newWorkout == false & renderWorkoutSelected == 'Run') {
+        pageLoad();
         return (
             <Card h='1000px' w='750px' boxShadow='lg' borderStyle='solid' borderWidth='5px' borderColor='#c9def7' bgColor='white'>
                 <CardHeader fontSize='75px' color='blue.800' fontWeight='bold' mt='100px' textShadow='3px 2px 2px #54D3B2'>Next Workout</CardHeader>
-                <CardHeader fontSize='35px' mb='25px' color='blue.800' fontWeight='bold'>Upcoming workout details</CardHeader>
+                <CardHeader fontSize='35px' color='blue.800' fontWeight='bold'>Upcoming workout details</CardHeader>
+                <CardHeader fontSize='40px' mb='25px' color='#54D3B2' fontWeight='bold'>{renderWorkoutSelected}</CardHeader>
+                <Container mb='75px' h='250px'>
+                    <Text fontSize='27.5px' color='blue.800'>{renderRunDetails}</Text>
+                </Container>
+                <Container>
+                    <Button h='100px' w='260px' mr='8px' bgColor='#c9def7' color='blue.800' fontSize='35px' fontWeight='bold' variant='outline' borderWidth='1px' borderColor='blue.800'
+                    onClick={handleNewWorkout}>
+                        New Workout
+                    </Button>
+                </Container>
+            </Card>
+        )
+    } else if (newWorkout == false & renderWorkoutSelected == 'Lift') {
+        return (
+            <Card h='1000px' w='750px' boxShadow='lg' borderStyle='solid' borderWidth='5px' borderColor='#c9def7' bgColor='white'>
+                <CardHeader fontSize='75px' color='blue.800' fontWeight='bold' mt='100px' textShadow='3px 2px 2px #54D3B2'>Next Workout</CardHeader>
+                <CardHeader fontSize='35px' color='blue.800' fontWeight='bold'>Upcoming workout details</CardHeader>
+                <CardHeader fontSize='40px' mb='25px' color='#54D3B2' fontWeight='bold'>{renderWorkoutSelected}</CardHeader>
                 <Container mb='75px'>
-                    <Text fontSize='27.5px' color='blue.800'>Bench -- TM190</Text>
-                    <Text fontSize='27.5px' color='blue.800'>Overhead Press -- TM125</Text>
-                    <Text fontSize='27.5px' color='blue.800'>Incline Press -- 3x6</Text>
-                    <Text fontSize='27.5px' color='blue.800'>Seated Row -- 3x12</Text>
-                    <Text fontSize='27.5px' color='blue.800'>Fly Machine -- 3x15</Text>
-                    <Text fontSize='27.5px' color='blue.800'>Curls -- 3x20</Text>
-                    <Text fontSize='27.5px' color='blue.800'>Skullcrushers -- 3x8</Text>
+                    <Text fontSize='27.5px' color='blue.800'>{renderLift1} -- {renderLift1Details}</Text>
+                    <Text fontSize='27.5px' color='blue.800'>{renderLift2} -- {renderLift2Details}</Text>
+                    <Text fontSize='27.5px' color='blue.800'>{renderLift3} -- {renderLift3Details}</Text>
+                    <Text fontSize='27.5px' color='blue.800'>{renderLift4} -- {renderLift4Details}</Text>
+                    <Text fontSize='27.5px' color='blue.800'>{renderLift5} -- {renderLift5Details}</Text>
+                    <Text fontSize='27.5px' color='blue.800'>{renderLift6} -- {renderLift6Details}</Text>
                 </Container>
                 <Container>
                     <Button h='100px' w='260px' mr='8px' bgColor='#c9def7' color='blue.800' fontSize='35px' fontWeight='bold' variant='outline' borderWidth='1px' borderColor='blue.800'
@@ -90,7 +209,7 @@ function NewWorkout () {
                 <CardHeader fontSize='75px' color='blue.800' fontWeight='bold' mt='25px' textShadow='3px 2px 2px #54D3B2'>Next Workout</CardHeader>
                 <CardHeader fontSize='35px' mb='25px' color='blue.800'>Please provide the run details</CardHeader>
                 <Container>
-                    <Input variant='outline' mb='75px' h='250px' outlineColor='black' fontSize='25px' align='center' justify='center'></Input>
+                    <Input variant='outline' mb='75px' h='250px' outlineColor='black' fontSize='25px' align='center' justify='center' onChange={(e) => setRunDetails(e.target.value)}></Input>
                 </Container>
                 <Container>
                     <Button h='90px' w='180px' mr='8px' color='blue.800' bgColor='#c9def7' borderWidth='1px' borderColor='blue.800' fontSize='35px' fontWeight='bold' mt='15px'
@@ -109,28 +228,28 @@ function NewWorkout () {
                 <CardHeader fontSize='75px' color='blue.800' fontWeight='bold' textShadow='3px 2px 2px #54D3B2'>Next Workout</CardHeader>
                 <CardHeader fontSize='35px' mb='25px' color='blue.800'>Please provide the lift details</CardHeader>
                 <Container>
-                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' mr='15px'></Input>
-                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' ml='15px'></Input>
+                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' mr='15px' onChange={(e) => setLift1(e.target.value)}></Input>
+                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' ml='15px' onChange={(e) => setLift1Details(e.target.value)}></Input>
                 </Container>
                 <Container>
-                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' mr='15px'></Input>
-                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' ml='15px'></Input>
+                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' mr='15px' onChange={(e) => setLift2(e.target.value)}></Input>
+                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' ml='15px' onChange={(e) => setLift2Details(e.target.value)}></Input>
                 </Container>
                 <Container>
-                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' mr='15px'></Input>
-                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' ml='15px'></Input>
+                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' mr='15px' onChange={(e) => setLift3(e.target.value)}></Input>
+                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' ml='15px' onChange={(e) => setLift3Details(e.target.value)}></Input>
                 </Container>
                 <Container>
-                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' mr='15px'></Input>
-                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' ml='15px'></Input>
+                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' mr='15px' onChange={(e) => setLift4(e.target.value)}></Input>
+                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' ml='15px' onChange={(e) => setLift4Details(e.target.value)}></Input>
                 </Container>
                 <Container>
-                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' mr='15px'></Input>
-                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' ml='15px'></Input>
+                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' mr='15px' onChange={(e) => setLift5(e.target.value)}></Input>
+                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' ml='15px' onChange={(e) => setLift5Details(e.target.value)}></Input>
                 </Container>
                                 <Container>
-                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' mr='15px'></Input>
-                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' ml='15px'></Input>
+                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' mr='15px' onChange={(e) => setLift6(e.target.value)}></Input>
+                    <Input variant='outline' mb='20px' h='50px' w='200px' outlineColor='black' ml='15px' onChange={(e) => setLift6Details(e.target.value)}></Input>
                 </Container>
                 <Container>
                     <Button h='90px' w='180px' mr='8px' color='blue.800' bgColor='#c9def7' fontSize='35px' fontWeight='bold' borderWidth='1px' borderColor='blue.800' mt='15px'
--- frontend/src/components/Log/WorkoutLog.jsx
+++ frontend/src/components/Log/WorkoutLog.jsx
@@ -19,59 +19,43 @@ function WorkoutLog () {
     const [averageSpeed, setAverageSpeed] = useState();
     const [maxSpeed, setMaxSpeed] = useState();
     const [workoutData, setWorkoutData] = useState();
-    
-    const currentTime = Date.now
-    const [tokenExpiration, setTokenExpiration] = useState();
+    const [emoji, setEmoji] = useState()
 
-    const emjoi = [
-        {
-            "Run": "🏃"
-        },
-        {
-            "Weight Training": "🏋️‍♂️"
+    const emojiChange = () => {
+
+        if (workoutData.sportType == "Run") {
+            setEmoji("🏃")
+        } else if (workoutData.sportType == "WeightTraining") {
+            setEmoji("🏋️‍♂️")
         }
-    ]
+    }
+
+    const currentTime = Date.now()
+    const [tokenExpiration, setTokenExpiration] = useState(0);
 
     const workoutDetails = [
         {
-            "activityId": 106942755,
-            "activityName": "Morning Run",
-            "startDate": "2024-06-16T10:47:23Z",
-            "sportType": "Run",
-            "description": "Run",
-            "distance": "6438.0",
-            "movingTime": "2342",
-            "averageSpeed": "2.749",
-            "maxSpeed": "5.494",
+            "activityId": 10817212404,
+            "activityName": "Upper Body Workout",
+            "startDate": "2023-10-20T17:47:21Z",
+            "sportType": "WeightTraining",
+            "description": "Bench (3x5) 115* Inc DB press (3x10) 35* row (3x5) 50 Lat pull down (3x10) 42.5* Over press (5x8) - 35* Curl 25db Tri pull down (3x10) 35",
+            "distance": "20.0",
+            "movingTime": "3600",
+            "averageSpeed": "0.0",
+            "maxSpeed": "0",
         }
     ]
 
-    const postWorkoutDetails = async () => {
+    const checkToken = () => {
 
-        console.log(workoutDetails)
-
-        axios({
-            method: 'post',
-            url: 'http://127.0.0.1:8000/api/workoutLog',
-            data: workoutDetails[0],
-        }).then((response) => {
-            console.log(response.data)
-        })
-    }
-
-    const pageLoad = () => {
-        activityRequest();
-    }
-
-    const getWorkoutDetails = async () => {
-
-        axios({
-            method: 'get',
-            url: 'http://127.0.0.1:8000/api/workoutLog'
-        }).then((response) => {
-            setWorkoutData(response.data)
-            console.log(workoutData)
-        })
+        console.log(currentTime)
+        console.log(tokenExpiration)
+        if (currentTime > tokenExpiration) {
+            getNewToken();
+        } else {
+            activityRequest();
+        }
     }
 
     const getNewToken = () => {
@@ -80,30 +64,40 @@ function WorkoutLog () {
             method: 'post',
             url: `https://www.strava.com/oauth/token?client_id=${clientID}&client_secret=${clientSecret}&grant_type=refresh_token&refresh_token=${refreshToken}&scope=activity:read_all`
         }).then((response) => {
-            console.log(response)
-        })
+            setCurrentToken(response.data.access_token)
+            setRefreshToken(response.data.refresh_token)
+            setTokenExpiration(response.data.expires_at)
+        }).then(
+            activityRequest()
+        )
     }
+
+    const pageLoad = () => {
+        postWorkoutDetails();
+        checkToken();
+        getWorkoutDetails()
+    }
+
 
     const activityRequest = async () => {
          
-            axios({
-                method: 'get',
-                url: `https://www.strava.com/api/v3/athlete/activities?access_token=${currentToken}`,
-            }).then((response) => {
-                console.log(response)
-                setActivityId(response.data[0].id)
-                console.log(activityId)
-                setRequest("Received")
-            }).catch((error) => {
-                console.log(error)
-            })
+        axios({
+            method: 'get',
+            url: `https://www.strava.com/api/v3/athlete/activities?access_token=${currentToken}`,
+        }).then((response) => {
+            console.log(response)
+            setActivityId(response.data[0].id)
+            console.log(activityId)
+        }).catch((error) => {
+            console.log(error)
+        })
     }
 
-    {/*const activityDetails = async () => {
+    const activityDetails = async () => {
 
         await axios({
             method: 'get',
-            url: `https://www.strava.com/api/v3/activities/${activityID}?include_all_efforts=""&access_token=999d8613243f5f0f350682d04de4673ab4bcbd73`,
+            url: `https://www.strava.com/api/v3/activities/${activityId}?include_all_efforts=""&access_token=${currentToken}`,
         }).then((response) => {
             
             console.log(response)
@@ -120,9 +114,32 @@ function WorkoutLog () {
 
     } 
 
-    useEffect(() => { 
-        activityRequest();
-    },[]) */}
+    const postWorkoutDetails = async () => {
+
+        console.log(workoutDetails[0])
+
+        axios({
+            method: 'post',
+            url: 'http://127.0.0.1:8000/api/workoutLog',
+            data: workoutDetails[0],
+        }).then((response) => {
+            console.log(response.data)
+        })
+    }
+
+
+    const getWorkoutDetails = async () => {
+
+        await axios({
+            method: 'get',
+            url: 'http://127.0.0.1:8000/api/workoutLog'
+        }).then((response) => {
+            setWorkoutData(response.data)
+            setRequest("Received")
+            console.log(workoutData)
+        })
+    }
+
 
     {/*postWorkoutDetails()*/}
 
@@ -131,28 +148,38 @@ function WorkoutLog () {
     },[])
 
 
-    return (
-            <Center>
-            <SimpleGrid columns={2} p={5} m='25px'>
-                {workoutDetails.map((workout) => (
-                    <Card key={workout} h='800px' w='700px'  m='50px' align='center' justify='center' borderStyle='solid' borderWidth='4px' borderColor='#1b305b' bgColor='#f4f6fa'>
-                        <CardHeader mt='25px'>
-                        <Heading fontSize='60px' color='#1b305b'> {workout.activityName}</Heading>
-                        </CardHeader>
-                        <CardBody>
-                            <Text fontSize='50px' mb='25px'>🏋️</Text>
-                            <Text fontSize='35px' fontWeight='bold' color='#1b305b'>Date</Text>
-                            <Text fontSize='30px' mb='25px'>{workout.startDate}</Text>
-                            <Text fontSize='35px' fontWeight='bold' color='#1b305b'>Workout Type</Text>
-                            <Text fontSize='30px' mb='25px'>{workout.sportType}</Text>
-                            <Text fontSize='35px' fontWeight='bold' color='#1b305b'>Workout Details</Text>
-                            <Text fontSize='30px' mb='25px'>{workout.description}</Text>
-                        </CardBody>
-                    </Card>
-                ))}
-            </SimpleGrid>
-            </Center>
-    )
-}
+
+
+    if (request == "Received") {
+        return (
+                <Center>
+                <SimpleGrid columns={2} p={5} m='25px'>
+                    {workoutData.map((workout) => (
+                        <Card key={workout} h='1000px' w='850px'  m='50px' align='center' justify='center' borderStyle='solid' borderWidth='4px' borderColor='#1b305b' bgColor='#f4f6fa'>
+                            <CardHeader mt='25px'>
+                            <Heading fontSize='60px' color='#1b305b'> {workout.activityName}</Heading>
+                            </CardHeader>
+                            <CardBody>
+                                <Text fontSize='50px' mb='25px'>{workout.sportType=="Run" ? ("🏃") : ("🏋️‍♂️")}</Text>
+                                <Text fontSize='35px' fontWeight='bold' color='#1b305b'>Date</Text>
+                                <Text fontSize='30px' mb='25px'>{workout.startDate}</Text>
+                                <Text fontSize='35px' fontWeight='bold' color='#1b305b'>Workout Type</Text>
+                                <Text fontSize='30px' mb='25px'>{workout.sportType}</Text>
+                                <Text fontSize='35px' fontWeight='bold' color='#1b305b'>{workout.sportType=="Run" ? ("Distance") : ("Description")}</Text>
+                                <Text fontSize='30px' mb='25px'>{workout.sportType=="Run" ? (`${workout.distance}`) : (`${workout.description}`)}</Text>
+                                <Text fontSize='35px' fontWeight='bold' color='#1b305b'>{workout.sportType=="Run" ? ("Elapsed Time") : ("")}</Text>
+                                <Text fontSize='30px' mb='25px'>{workout.sportType=="Run" ? (`${workout.movingTime}`) : ("")}</Text>
+                                <Text fontSize='35px' fontWeight='bold' color='#1b305b'>{workout.sportType=="Run" ? ("Max Speed") : ("")}</Text>
+                                <Text fontSize='30px' mb='25px'>{workout.sportType=="Run" ? (`${workout.maxSpeed}`) : ("")}</Text>
+                                <Text fontSize='35px' fontWeight='bold' color='#1b305b'>{workout.sportType=="Run" ? ("Average Speed") : ("")}</Text>
+                                <Text fontSize='30px' mb='25px'>{workout.sportType=="Run" ? (`${workout.averageSpeed}`) : ("")}</Text>
+                            </CardBody>
+                        </Card>
+                    ))}
+                </SimpleGrid>
+                </Center>
+        )
+    } 
+} 
 
 export default WorkoutLog
